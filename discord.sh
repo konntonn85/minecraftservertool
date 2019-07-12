8afc637a459c30c918f342dfd273c3034007a1f8
konntonn85 <konntonn85@outlook.jp>
2019-07-12 20:43:32 +0900

    Create discord.sh
    とりあえず完成版からあげる
    使うときはURL修正してechoで内容ぶち込んで使う
    コメント英語にしたのは失敗だったかもしれない

---
+++ discord.sh
@@ -0,0 +1,37 @@
+set -eu
+
+# save temporaly
+MESSAGEFILE=$(mktemp -t webhooksXXXX)
+# after finish remove 
+trap "rm ${MESSAGEFILE}" 0
+
+# with or not pipe
+if [ -p /dev/stdin ]; then
+    # exchange new line code and save
+    cat - | tr '\n' '\\' | sed 's/\\/\\n/g' > ${MESSAGEFILE}
+else
+    echo "nothing stdin"
+    exit 1
+fi
+
+# URL of Web hook
+URL='hogehoge.piyopiyo.com'
+# channel to export
+CHANNEL=${CHANNEL:-'minecraft'}
+# name of bot
+BOTNAME=${BOTNAME:-'okutani-bot'}
+# emoji
+EMOJI=${EMOJI:-':sushi:'}
+# head tittle
+HEAD=${HEAD:-"[server message]\n"}
+
+# get message with sintax highlight
+MESSAGE='```'`cat ${MESSAGEFILE}`'```'
+
+# make json
+payload="payload={
+      \"text\": \"${HEAD}${MESSAGE}\"
+}"
+
+# export 
+curl -s -S -X POST --data-urlencode "${payload}" ${URL} > /dev/null
